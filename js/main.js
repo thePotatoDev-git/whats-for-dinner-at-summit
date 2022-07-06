fa94bef86668dd4d5909108010f67160bb2237ae
Albertus Chua <albertus.chua@gmail.com>
2022-07-06 17:28:30 -0400

    Changed from randomizer to head to head style choices

--- js/main.js
+++ js/main.js
@@ -1,25 +1,43 @@
 document.querySelector('.low').addEventListener('click', lowBudget);
 document.querySelector('.mid').addEventListener('click', midBudget);
 document.querySelector('.high').addEventListener('click', highBudget);
+document.querySelector('#option1').addEventListener('click', chooseOption1);
+document.querySelector('#option2').addEventListener('click', chooseOption2);
 
+
+const lowBudgetOptions = ['Burger King', 'Aldi Pizza', 'Aldi Stroganoff', 'Aldi salad and chicken', 'Taco Bell'];
+const midBudgetOptions = ['Sarku', 'Charley\'s Cheesesteaks', 'Smash Burger', 'Eon\'s', 'Juicy Platters', 'Bahama Breeze', 'Chipotle'];
+const highBudgetOptions = ['Mama Chicken', 'Katsune', 'Woodstone Pizza', 'Tavolino\'s Pizza', 'Blackbeard Ramen', 'Jim Bob\'s', 'Rebar'];
+let foodOptions = [];
 
 function lowBudget() {
-    const lowBudgetOptions = ['Burger King', 'Aldi Pizza', 'Aldi Stroganoff', 'Aldi salad and chicken', 'Taco Bell'];
-    const randomizeLowBudget = Math.floor(Math.random() * lowBudgetOptions.length);
-    
-    document.querySelector('.dinner').innerText = lowBudgetOptions[randomizeLowBudget];
+    // Round one options
+    foodOptions = lowBudgetOptions;
+    console.log(foodOptions);
+    document.querySelector('#option1').innerHTML = foodOptions[0];
+    document.querySelector('#option2').innerHTML = foodOptions[foodOptions.length - 1];
+
 }
 
 function midBudget() {
-    const midBudgetOptions = ['Sarku', 'Charley\'s Cheesesteaks', 'Smash Burger', 'Eon\'s', 'Juicy Platters', 'Bahama Breeze', 'Chipotle'];
-    const randomizeMidBudget = Math.floor(Math.random() * midBudgetOptions.length);
-
-    document.querySelector('.dinner').innerText = midBudgetOptions[randomizeMidBudget];
+    foodOptions = midBudgetOptions;
+    document.querySelector('#option1').innerHTML = foodOptions[0];
+    document.querySelector('#option2').innerHTML = foodOptions[foodOptions.length - 1];
 }
 
 function highBudget() {
-    const highBudgetOptions = ['Mama Chicken', 'Katsune', 'Woodstone Pizza', 'Tavolino\'s Pizza', 'Blackbeard Ramen', 'Jim Bob\'s', 'Rebar']
-    const randomizeHighBudget = Math.floor(Math.random() * highBudgetOptions.length);
+    document.querySelector('#option1').innerHTML = foodOptions[0];
+    document.querySelector('#option2').innerHTML = foodOptions[foodOptions.length - 1];
+}
 
-    document.querySelector('.dinner').innerText = highBudgetOptions[randomizeHighBudget]
+function chooseOption1() {
+    foodOptions.pop();
+    console.log(foodOptions);
+    document.querySelector('#option2').innerHTML = foodOptions[foodOptions.length - 1];
+}
+
+function chooseOption2() {
+    foodOptions.shift();
+    console.log(foodOptions);
+    document.querySelector('#option1').innerHTML = foodOptions[0];
 }
